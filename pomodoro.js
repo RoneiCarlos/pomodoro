66b5429521ed8caca4631b5fb1674ef432c6fe18
Ronei Carlos <41250984+RoneiCarlos@users.noreply.github.com>
2020-12-23 13:00:23 -0300

    funcionando em uma instância

--- pomodoro.js
+++ pomodoro.js
@@ -2,6 +2,49 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 
+var situacao = 0;
+var sit = situacao;
+var rodando = false;
+var min_foco = null;
+var min_pausa = null;
+var tempo = null;
+mensagem = null;
+
+function funcao(){
+    if (rodando) {
+        if (situacao == 1){
+            if (tempo > 0) {
+                tempo -= 1;
+                mensagem.edit(`Tempo de foco restante: ${tempo}`);
+                sit = situacao;
+            }
+            else {
+                situacao = 2;
+                tempo = min_pausa;
+                mensagem.edit(`Tempo de pausa: ${tempo}`);
+                sit = situacao;
+            }
+            return;
+        } else if (situacao == 2) {
+            if (tempo > 0) {
+                tempo -= 1;
+                mensagem.edit(`Tempo de pausa restante: ${tempo}`);
+                sit = situacao;
+            }
+            else {
+                situacao = 1;
+                tempo = min_foco;
+                mensagem.edit(`Tempo de foco: ${tempo}`);
+                sit = situacao;
+            }
+            return;
+        } 
+    }
+        
+} 
+
+var intervalo = setInterval(funcao, 1000);
+
 client.on("ready", () => {
     client.user.setActivity("+help", { type: "LISTENING"})
 });
@@ -11,11 +54,12 @@ client.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
     if(!message.content.startsWith(config.prefix)) return;
+    
 
     const args = message.content.slice(config.prefix.length).trim().split(" ");
-    const comando = args.shift().toLowerCase(); // +"init" 50 5 lofi radio ... ... ...
-    var min_foco = args[0];                   // +init "50" 5 lofi radio ... ... ...
-    var min_pausa = args[1];                  // +init 50 "5" lofi radio ... ... ...
+    const comando = args.shift().toLowerCase(); 
+    min_foco = args[0];
+    min_pausa = args[1];
     
     var pesquisa = "";
     for (i in args) {
@@ -23,34 +67,36 @@ client.on("message", async message => {
             pesquisa = pesquisa.concat(args[i] + " ");
         }
     }
-    pesquisa = pesquisa.trim();                // +init 50 5 "lofi radio ... ... ..."
-
+    pesquisa = pesquisa.trim();
     
-    
+    tempo = min_foco;
 
     if(comando === "init") {
-        message.member.voice.channel.join(); 
-    }
-    else if(comando === "help") {
-        message.channel.send("`+help`: lista de comandos\n`+init X Y`: inicia o bot e define o tempo de foco(`X`) e de pausa(`Y`) em minutos\n`+pause`: pausa o timer\n`+stop`: interrompe o timer\n`+disconnect`: desconecta o bot do canal");
-    }
-    else if(comando === "pause") {
-
+        if (args[0] === undefined || args[1] === undefined) {
+            mensagem = await message.channel.send("Comando `+init` precisa de argumentos completos.\nDigite `+help` para exemplo.");
+        } else {
+            situacao = 1;
+            rodando = true;
+            mensagem = await message.channel.send(`${message.author} Tempo de foco: ${min_foco} tempo de pausa: ${min_pausa}`);
+            intervalo;
+        }
+        
     }
     else if(comando === "stop") {
-
+        if (!rodando) {
+            mensagem = await message.channel.send("Comando `+stop` só funciona se houver um pomodoro ativo.");
+        } else {
+            rodando = false;
+            clearInterval(intervalo);
+            mensagem = await message.channel.send(`${message.author} Timer encerrado!`);
+        }
     }
-    else if(comando === "disconnect") {
-        message.member.voice.channel.leave();
+    else if(comando === "help") {
+        message.channel.send("`+help`: lista de comandos\n`+init X Y`: inicia o bot e define o tempo de foco(`X`) e de pausa(`Y`) em minutos\n`+stop`: interrompe o timer e desconecta do canal de voz");
+    }
+    else {
+        message.channel.send("Comando inválido\n`+help`: lista de comandos\n`+init X Y`: inicia o bot e define o tempo de foco(`X`) e de pausa(`Y`) em minutos\n`+stop`: interrompe o timer e desconecta do canal de voz");
     }
 });
 
 client.login(config.token);
-
-// ao entrar no server, se não tiver um canal de chat e de voz chamados "pomodoro", criar ambos
-
-// formato comando init = +init {minutos concentração} {minutos pausa} {opcional| url de musica pro groovy iniciar junto com o pomodoro }
-    // se o comando init for chamado por um usuário que estiver fora de um canal de voz ele vai:
-        // entrar no canal de voz pomodoro
-        // chamar o usuário que mandou o comando init
-        // chamar o groovy
